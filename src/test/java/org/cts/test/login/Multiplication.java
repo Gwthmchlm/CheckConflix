cb8640cc63e8e44e18de1929ca0a6ef861dfae2e
Gwthmchlm <gowthamkalyan17@gmail.com>
2019-12-16 16:47:36 +0530

    Number reverse program

---
+++ src/test/java/org/cts/test/login/Multiplication.java
@@ -0,0 +1,14 @@
+package org.cts.test.login;
+
+public class Multiplication {
+public static void main(String[] args) {
+	int i = 0, j = 0, a = 0, n=12321;
+	a = n;
+	while (a>0) {
+		i = a%10;
+		j = (j*10)+i;
+		a = a/10;
+	}
+	System.out.println(j);
+}
+}
